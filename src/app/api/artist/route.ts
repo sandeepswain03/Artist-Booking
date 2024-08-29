b0ae9f4c307f0292e2ea2b2f35b15fe67ad5a1d9
Parag0712 <paragvadgama123@gmail.com>
2024-08-29 23:33:54 +0530

    artist rating added

--- src/app/api/artist/route.ts
+++ src/app/api/artist/route.ts
@@ -1,6 +1,8 @@
 import UserModel from "@/models/User.model";
 import dbConnect from "@/lib/dbConnect";
 import { NextResponse } from "next/server";
+import { Schema } from "mongoose";
+import { getServerSession } from "next-auth";
 
 export async function GET(request: Request) {
   await dbConnect();
@@ -19,3 +21,75 @@ export async function GET(request: Request) {
     );
   }
 }
+
+
+// Patch  Request 
+export async function PATCH(request: Request) {
+  await dbConnect();
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return NextResponse.json(
+        { success: false, message: "Not authenticated" },
+        { status: 401 }
+      );
+    }
+    const userId = session.user._id;
+    const { artistId, rating } = await request.json();
+
+    if (!artistId || !userId || rating === undefined) {
+      return NextResponse.json(
+        { success: false, message: "Invalid input data" },
+        { status: 400 }
+      );
+    }
+
+    // Check if the artist (user with role "artist") exists
+    const artist = await UserModel.findOne({ _id: artistId, role: "artist" });
+    if (!artist) {
+      return NextResponse.json(
+        { success: false, message: "Artist not found" },
+        { status: 404 }
+      );
+    }
+
+    // Check if the user has already rated the artist
+    const existingRating = artist.rating?.ratings.find(
+      (r) => r.userId.toString() === userId
+    );
+
+    if (existingRating) {
+      // Update the existing rating
+      existingRating.rating = rating;
+    } else {
+      // Add the new rating
+      artist.rating?.ratings.push({
+        userId: new Schema.Types.ObjectId(userId),
+        rating,
+      });
+      artist.rating!.count += 1; // Increment the rating count
+    }
+
+    // Recalculate the average rating
+    const totalRatings = artist.rating?.ratings.reduce(
+      (acc, curr) => acc + curr.rating,
+      0
+    );
+    artist.rating!.average = totalRatings! / artist.rating!.count;
+
+    // Save the updated artist
+    await artist.save();
+
+    return NextResponse.json(
+      { success: true, data: artist.rating },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error updating rating:", error);
+    return NextResponse.json(
+      { success: false, message: "Failed to update rating" },
+      { status: 500 }
+    );
+  }
+}
+
