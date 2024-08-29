894a85de7690401573981b835b72c50537ea826f
Parag0712 <paragvadgama123@gmail.com>
2024-08-29 23:34:05 +0530

    artist rating added

--- src/app/api/artist/route.ts
+++ src/app/api/artist/route.ts
@@ -3,6 +3,7 @@ import dbConnect from "@/lib/dbConnect";
 import { NextResponse } from "next/server";
 import { Schema } from "mongoose";
 import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
 
 export async function GET(request: Request) {
   await dbConnect();
